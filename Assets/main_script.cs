f972c9d9674bfa1d243331ab0b000761b1813657
Bell-Herald <bahill4@wisc.edu>
2024-10-05 17:15:32 -0500

    Current state with working stars and planets

--- Assets/main_script.cs
+++ Assets/main_script.cs
@@ -4,51 +4,40 @@ using UnityEngine;
 
 public class main_script : MonoBehaviour
 {
+    //Public variables used for initialization
     public ParticleSystem particle_system_prefab;
+    public int star_count = 118218;
+    public int planet_count = 5765;
     public Camera camera_prefab;
+    public TextAsset star_data;
+    public TextAsset exoplanet_data;
+
+    //private variables used internally
     private Camera camera;
     public GameObject planet_prefab;
     private ParticleSystem particle_system;
     private ParticleSystem.Particle[] particles;
-
     private Vector3 look;
     private Vector3 camera_offset;
     private Vector3 last_location;
     private Vector3 next_location;
     private float percent_travelled;
     private float travel_seconds;
+    private float look_speed;
+    private float travel_power;
+    private int distance_multiplier;
+    private float lookSpeed;
+    private float size_of_earth;
+    private Vector3[] planets;
 
     // Start is called before the first frame update
     void Start()
     {
-        camera = Instantiate(camera_prefab);
-        Vector3 look = new Vector3(0,0,0);
-        camera_offset = new Vector3(0, 5.4f, 0);
-        camera.transform.position = camera_offset;
-        next_location = camera_offset;
-        percent_travelled = 1;
-        travel_seconds = 10f;
+        initialize_globals();
+        initialize_camera();
 
-        particle_system = Instantiate(particle_system_prefab);
-        Vector3[] locations = get_locations(100000, -1000, 1000); //1000000, -1000, 1000
-        int num_particles = locations.Length;
-        particle_system.Emit(num_particles);
-        particles = new ParticleSystem.Particle[num_particles];
-        int num_particles2 = particle_system.GetParticles(particles);
-        for(int i = 0; i < num_particles; i ++) {
-            particles[i].position = locations[i];
-        }
-        particle_system.SetParticles(particles);
-
-        Vector3[] planet_locations = get_locations(100, -1000, 1000); 
-        for(int i = 0; i < planet_locations.Length; i ++) {
-            GameObject planet = Instantiate(planet_prefab);
-            planet.transform.position = planet_locations[i];
-            planet.GetComponent<planet_script>().main = this;
-        }
-        GameObject my_planet = Instantiate(planet_prefab);
-        my_planet.transform.position = new Vector3(0,0,0);
-        my_planet.GetComponent<planet_script>().main = this;
+        initialize_particles();
+        initialize_planets();
     }
 
     private Vector3[] get_locations(int num_locations, int min_val, int max_val) {
@@ -62,27 +51,70 @@ public class main_script : MonoBehaviour
 
     // Update is called once per frame
     void Update() {
-        /*
-        float lookSpeed = 1.5f;
-        float y = Input.GetAxis("Mouse X");
-        float x = Input.GetAxis("Mouse Y");
-        Debug.Log(x + ":" + y);
-        Vector3 rotateValue = new Vector3(x * lookSpeed, y * -1 * lookSpeed, 0);
-        camera.transform.eulerAngles = transform.eulerAngles - rotateValue;
-        */
+        update_camera();
+    }
 
-        float lookSpeed = 1.5f;
+    private void initialize_particles() {
+        particle_system = Instantiate(particle_system_prefab);
+//        particle_system.Emit(star_count);
+        particles = new ParticleSystem.Particle[star_count];
+        particle_system.GetParticles(particles);
+        int particle_count = 0;
+        CSVParser.Parse(star_data, 0, 2, (location) => {
+            particles[particle_count].position = new Vector3(location[0] * distance_multiplier, location[1] * distance_multiplier, location[2] * distance_multiplier);
+            particle_count ++;
+        });
+        particle_system.SetParticles(particles);
+        
+    }
 
+    private void initialize_planets() {
+        planets = new Vector3[planet_count];
+        int sphere_count = 0;
+        CSVParser.Parse(exoplanet_data, 1, 3, (location) => {
+            GameObject planet = Instantiate(planet_prefab);
+            planet.transform.position = new Vector3(location[1] * distance_multiplier, location[2] * distance_multiplier, location[3] * distance_multiplier);
+            planet.transform.localScale = new Vector3(size_of_earth * distance_multiplier, size_of_earth * distance_multiplier, size_of_earth * distance_multiplier);
+            planet.GetComponent<planet_script>().main = this;
+            planets[sphere_count] = new Vector3(location[1], location[2], location[3]);
+            sphere_count += 1;
+        });
+
+
+//        for(int i = 0; i < planet_locations.Length; i ++) {
+//            GameObject planet = Instantiate(planet_prefab);
+//            planet.transform.position = planet_locations[i];
+//            planet.GetComponent<planet_script>().main = this;
+//        }
+
+//        GameObject my_planet = Instantiate(planet_prefab);
+//        my_planet.transform.position = new Vector3(0,0,0);
+//        my_planet.GetComponent<planet_script>().main = this;
+    }
+    private void initialize_globals() {
+        distance_multiplier = 10;
+        size_of_earth = 0.1f; //0.000000002f;
+    }
+    private void initialize_camera() {
+
+        camera = Instantiate(camera_prefab);
+        Vector3 look = new Vector3(0,0,0);
+        camera_offset = new Vector3(0, size_of_earth * distance_multiplier + 0.4f, 0);
+        camera.transform.position = camera_offset;
+        next_location = camera_offset;
+        percent_travelled = 1;
+        travel_seconds = 10f;
+        look_speed = 1.5f;
+        travel_power = 2f;
+        lookSpeed = 1.5f;
+    }
+    private void update_camera() {
         look = look + new Vector3(-Input.GetAxis("Mouse Y") * lookSpeed, Input.GetAxis("Mouse X") * lookSpeed, 0);
-//        look.x = Mathf.Clamp(look.x, -80, 80);
         camera.transform.eulerAngles = look;
-        //camera.transform.position = position;
-
         if(percent_travelled != 1) {
             percent_travelled = Mathf.Min(1, percent_travelled + Time.deltaTime / travel_seconds);
             float power = 2;
             float eased_percent_travelled = Mathf.Pow((1 - Mathf.Cos(percent_travelled * Mathf.PI))/ 2, power);
-//            camera.transform.position = next_location * percent_travelled + last_location * (1 - percent_travelled);
             camera.transform.position = Vector3.Lerp(last_location, next_location, eased_percent_travelled);
         }
     }
